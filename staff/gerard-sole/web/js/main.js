a55a66688f8d07021d342626dd8adec683b6a301
gerard sole <gerardsoclu@gmail.com>
2021-10-06 09:17:49 +0200

    add js exercise

---
+++ staff/gerard-sole/web/js/main.js
@@ -0,0 +1,56 @@
+var manzana = {
+    name: "Manzana",
+    quantity: 5,
+    price: 2
+}
+var melon = {
+    name: "Melón",
+    quantity: 7,
+    price: 1
+}
+var uvas = {
+    name: "Uvas",
+    quantity: 2,
+    price: 6
+}
+var frutas = [manzana, melon, uvas];
+var createRows = function(arr) {
+    var rows = "";
+    for(var i = 0; i < arr.length; i++) {
+        var row =
+            "<tr>" +
+                "<td>" + arr[i].name + "</td>" +
+                "<td>" + arr[i].quantity + "</td>" +
+                "<td>" + arr[i].price + "</td>" +
+            "</tr>"
+        ;
+        
+        rows += row;
+    }
+    return "<tbody>"+rows+"</tbody>";
+}
+var title = 
+    "<thead>" +
+        "<tr>" +
+            "<th>Nombre</th>" +
+            "<th>Cantidad en kilos</th>" +
+            "<th>Precio de un kilo</th>" +
+        "</tr>" +
+    "</thead>"
+;
+var createFooter = function(arr) {
+    var totalPrice = 0;
+    for (let i = 0; i < arr.length; i++) {
+        var totalProductPrice = arr[i].quantity * arr[i].price;
+        totalPrice += totalProductPrice;
+    }
+    var footer =
+        "<tfoot>" +
+            "<tr>" +
+                "<td>total: "+totalPrice+" euros</td>" +
+            "</tr>" +
+        "</tfoot>"
+    ;
+    return footer;
+}
+document.write("<table>"+title+createRows(frutas)+createFooter(frutas)+"</table>")
