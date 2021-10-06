0470352e99a100f33351ba34e3cc2be989a0ba0b
gerard sole <gerardsoclu@gmail.com>
2021-10-06 22:21:18 +0200

    try js exercise

---
+++ staff/gerard-sole/web/js-exercices/numbers.js
@@ -0,0 +1,27 @@
+function numbers(numbers) { 
+    var totalPositives = 0;
+    var totalNegatives = 0;
+    var balance = 0;
+    
+    for(var i=0; i < values.length; i++) {
+        if(value > 0) {
+        totalPositives++
+        }
+        else {
+
+        totalNegatives++
+        }
+    }
+    var arrCount = [totalPositives, totalNegatives, balance];
+    return arrCount; 
+      
+}
+
+
+// La función debe devolver un array con 3 posiciones.
+
+// La primera posición debe ser la suma de los números positivos
+
+// La segunda posición debe ser la suma de los números negativos
+
+// La tercera posición debe ser el resultado de positivos - negativos
