6d0f328400dee25bef64b88c985ff3c7af792821
alvaro cortes <cortesalvaro01@gmail.com>
2021-10-06 10:24:39 +0200

    correct js

--- staff/alvaro-cortes/web/js/main.js
+++ staff/alvaro-cortes/web/js/main.js
@@ -6,33 +6,22 @@ var frutas = [bananas, oranges, kiwis]
 
 function createTable(array){
     var table = "<table><tr><th>Nombre</th><th>Cantidad</th><th>Precio</th><tr>"
-    var totalQuantity = 0;
-    var totalPrice = 0;
+    var total = 0;
 
     // Se crea la primera parte de la tabla con los titulos y los datos en el body del arreglo frutas //
 
     for(let i = 0; i < array.length; i++){
-
         table += "<td>" + array[i].name + "</td>" +
                  "<td>" + array[i].quantity + "</td>" +
                  "<td>" + array[i].price + "</td></tr>"
     }
 
-    // Generamos la suma total de las cantidaddes //
+    // Generamos la suma total //
 
     for(let i = 0; i < frutas.length; i++){
-        totalQuantity += frutas[i].quantity
+        total += frutas[i].price * frutas[i].quantity
     }
-
-    table += "<tr><th>Total</th><td>" + totalQuantity + "</td>"
-
-    // Generamos la suma total de los precios //
-
-    for(let i = 0; i < array.length; i++){
-        totalPrice += frutas[i].price 
-    }
-
-    table += "<td>" + totalPrice + "</td></tr>"
+    table += "<tr><th>Total</th><td>" + total + "</td></tr>"
 
     // Por último retornamos la variable table y la cerramos con el tag </table> //
 
