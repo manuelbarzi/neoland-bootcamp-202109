00072f196d8fb1ba3f0ddb260b6b0e0ab322350c
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2021-10-06 19:44:32 +0200

    add countValues y numbers

---
+++ staff/ventura-rodriguez/web/js/count-values.js
@@ -0,0 +1,23 @@
+function countValues(values) { // values es de tipo array
+    var totalItems = 0;
+    var totalTrue = 0;
+    var totalFalse = 0;
+
+    //
+    //
+    //
+
+    var arrCount = [totalItems, totalTrue, totalFalse];
+    return arrCount;
+}
+
+// La función debe devolver un array con 3 posiciones.
+
+// La primera posición debe ser el número total de elementos del array
+// que nos pasa el usuario
+
+// La segunda posición debe ser el número de elementos true que posee
+// el array
+
+// La tercera posición debe ser el número de elementos false que posee
+// el array
---
+++ staff/ventura-rodriguez/web/js/numbers.js
@@ -0,0 +1,20 @@
+function numbers(values) { // values es de tipo array
+    var totalPositives = 0;
+    var totalNegatives = 0;
+    var balance = 0;
+
+    //
+    //
+    //
+
+    var arrCount = [totalItems, totalTrue, totalFalse];
+    return arrCount;
+}
+
+// La función debe devolver un array con 3 posiciones.
+
+// La primera posición debe ser la suma de los números positivos
+
+// La segunda posición debe ser la suma de los números negativos
+
+// La tercera posición debe ser el resultado de positivos - negativos
