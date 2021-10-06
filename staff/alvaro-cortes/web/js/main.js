5c514a88e924a6eef0fbec12a51c327553959084
alvaro cortes <cortesalvaro01@gmail.com>
2021-10-06 10:39:18 +0200

    add css styles on js document

--- staff/alvaro-cortes/web/js/main.js
+++ staff/alvaro-cortes/web/js/main.js
@@ -5,13 +5,13 @@ var kiwis = { name: 'Kiwi', quantity: 2, price: 6 }
 var frutas = [bananas, oranges, kiwis]
 
 function createTable(array){
-    var table = "<table><tr><th>Nombre</th><th>Cantidad</th><th>Precio</th><tr>"
+    var table = "<table><thead><tr class='table__head__foot'><th>Nombre</th><th>Cantidad</th><th>Precio</th></thead>"
     var total = 0;
 
     // Se crea la primera parte de la tabla con los titulos y los datos en el body del arreglo frutas //
 
     for(let i = 0; i < array.length; i++){
-        table += "<td>" + array[i].name + "</td>" +
+        table += "<tr class='table'><td>" + array[i].name + "</td>" +
                  "<td>" + array[i].quantity + "</td>" +
                  "<td>" + array[i].price + "</td></tr>"
     }
@@ -21,7 +21,7 @@ function createTable(array){
     for(let i = 0; i < frutas.length; i++){
         total += frutas[i].price * frutas[i].quantity
     }
-    table += "<tr><th>Total</th><td>" + total + "</td></tr>"
+    table += "<tfoot><tr class='table__head__foot'><th colspan='2'>Total</th><td>" + total + "</td></tr></tfoot>"
 
     // Por último retornamos la variable table y la cerramos con el tag </table> //
 
