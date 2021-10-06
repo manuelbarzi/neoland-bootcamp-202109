c9445dfbc27b49bcbed0ba41baab5966fda82850
AnRo22 <anita220390@gmail.com>
2021-10-06 21:42:41 +0200

    Exercices of Ventu

---
+++ staff/ana-rodriguez/web/JavaScrip/js-exercises/js-exercises1/numbers.js
@@ -0,0 +1,30 @@
+function numbers(values) { // values es de tipo array
+    var totalPositives = 0;
+    var totalNegatives = 0;
+    var balance = 0;
+
+
+    var arrCount = [totalPositives, totalNegatives, balance];
+
+     for(var  i = 0; i < values.length; i++){
+         if ( values[i] > 0 ){
+            totalPositives ++
+         } else {
+             totalNegatives ++
+         }
+    }
+    
+    balance= totalPositives - totalNegatives
+    
+    arrCount = [totalPositives, totalNegatives, balance];
+
+    return arrCount;
+}
+
+// La función debe devolver un array con 3 posiciones.
+
+// La primera posición debe ser la suma de los números positivos
+
+// La segunda posición debe ser la suma de los números negativos
+
+// La tercera posición debe ser el resultado de positivos - negativos
