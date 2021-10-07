c68a8d030e53797c38865721747e349974455b94
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2021-10-07 18:31:36 +0200

    add new exercices

---
+++ staff/ventura-rodriguez/web/js/sexy-primes.js
@@ -0,0 +1,8 @@
+// Los primos sexy son pares de dos primos que están
+// separados por 6
+function sexyPrimes(a, b) {
+
+    // Implementa tu código aquí
+
+    return null // Debe devolver un true o false
+}
