6a12f5aafd3b0d45c0d42f86633ab3c585fe43d8
Noelia Saura <noelia.saura.m@gmail.com>
2021-10-14 18:24:02 +0200

    result search

--- staff/noelia-saura/web/js/js-exercises1/strings/search.js
+++ staff/noelia-saura/web/js/js-exercises1/strings/search.js
@@ -3,7 +3,26 @@
 //busca una palabra en el string y devuelve en donde se situa
 //devuelve el numero teniendo en cuenta cada letra del string
 
-function frases(frase, palabra) {
-    var search = frase.search(palabra)
-    return search
+// function frases(frase, palabra) {
+//     var search = frase.search(palabra)
+//     return search
+// }
+
+function search(text, str) {
+    var pos = -1
+    var j = 0
+    var found = false
+
+    for (let i = 0; i < text.length && found === false; i++) {
+        char = text[i]
+        if (char === str[j]) {
+            j++
+            if(j === str.length)
+            {
+                found = true
+                pos = i-j + 1
+            }
+        }else j = 0
+    }
+    return pos
 }
--- staff/noelia-saura/web/js/js-exercises1/strings/search.test.js
+++ staff/noelia-saura/web/js/js-exercises1/strings/search.test.js
@@ -4,31 +4,33 @@ describe ('TEST search')
 
 //case 0
 
-var frase = 'Noelia se encuentra estudiando con musica'
-var palabra = 'se'
+var text = 'Noelia se encuentra estudiando con musica'
 
-var res= frases(frase, palabra)
-if (res===7) {
+
+var res= search(text, 'se')
+if (typeof res === 'number'
+    &&res===7) {
     success('test ok')
 }else{
     fail('test failed')
 }
 
 //case 1
-var frase = 'Noelia se encuentra estudiando con musica'
-var palabra = 'con'
+var text = 'Noelia se encuentra estudiando con musica'
 
-var res= frases(frase, palabra)
-if (res===31) {
+var res= search(text, 'con')
+if (typeof res === 'number'
+    &&res===31) {
     success('test ok')
 }else{
     fail('test failed')
 }
 //CASE 2
-var frase = '1, 2, 3'
-var palabra = '2'
-var res= frases(frase, palabra)
-if (res=== 3) {
+var text = '1, 2, 3'
+
+var res= search(text, '2')
+if (typeof res === 'number'
+    &&res=== 3) {
     success('test ok')
 }else{
     fail('test failed')
