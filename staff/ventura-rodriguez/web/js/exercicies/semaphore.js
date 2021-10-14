6f7283b4aae718291e0c596ca27a067f56f1cfe9
Ventu Rodríguez <brodriguezserrano54@gmail.com>
2021-10-14 17:00:54 +0200

    fix exercise

--- staff/ventura-rodriguez/web/js/exercicies/semaphore.js
+++ staff/ventura-rodriguez/web/js/exercicies/semaphore.js
@@ -1,5 +1,8 @@
-function lightGreen(params) {
-    
+function lightGreen(person = "pass", light = "green", cars ="wait") {
+
+
+    return heart;
 }
 
 // Genera una función que devuelva si el peaton vive o muere (true o false)
+// Implementa la solución solamente con ifs ternarios
