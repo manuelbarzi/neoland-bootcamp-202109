84b430a8cf010ac35d8fb4306f1a640910e55d3a
AnRo22 <anita220390@gmail.com>
2021-10-17 20:54:13 +0200

    exercices string

---
+++ staff/ana-rodriguez/web/JavaScrip/js array-string/string/char-at.test.js
@@ -0,0 +1,29 @@
+describe('TEST charAt')
+ 
+
+describe('CASO 1')
+
+var str = 'Hello my friend';
+var res = str.charat(string,4)
+
+if(typeof res === 'string'
+    && res === 'o'){
+        success('test ok')
+     } else{
+        fail('test fail')
+       
+    }
+
+
+    describe('CASO ')
+
+    var str = 'Hello my friend';
+    var res = str.charat(string,6)
+    
+    if(typeof res === 'string'
+        && res === 'm'){
+            success('test ok')
+         } else{
+            fail('test fail')
+           
+        }   
