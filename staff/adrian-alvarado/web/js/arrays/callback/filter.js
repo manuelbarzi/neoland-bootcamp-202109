619edd1cbce46320062cbf51426f7864293eb24a
Adrian <eidrian.uct@gmail.com>
2021-10-18 10:52:33 +0200

    new test filter

--- staff/adrian-alvarado/web/js/arrays/callback/filter.js
+++ staff/adrian-alvarado/web/js/arrays/callback/filter.js
@@ -1,13 +1,29 @@
+// function filter(array, callback) {
+//     var result = []
+//     var j = 0
+
+//     for (let i = 0; i < array.length; i++) {
+//         let element = array[i];
+
+//         if ( callback(element, i) ) {
+//             result[j] = element
+//             j++
+//         }
+//     }
+
+//     return result
+// }
+
+
+
 function filter(array, callback) {
     var result = []
-    var j = 0
 
     for (let i = 0; i < array.length; i++) {
         let element = array[i];
-        
-        if ( callback(element, i) ) {
-            result[j] = element
-            j++
+
+        if (callback(element, i)) {
+            result[result.length] = element
         }
     }
 
--- staff/adrian-alvarado/web/js/arrays/callback/filter.test.js
+++ staff/adrian-alvarado/web/js/arrays/callback/filter.test.js
@@ -2,10 +2,10 @@ describe('TEST filter')
 
 describe('case 1')
 
-var array = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
+var text = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
 var items = []
 
-var res = filter(array, function (element, index) {
+var res = filter(text, function (element, index) {
     items[index] = element
 
     return element.length > 6
@@ -13,16 +13,48 @@ var res = filter(array, function (element, index) {
 
 if (res instanceof Array
     && res.length === 3
-    && res[0] === array[3]
-    && res[1] === array[4]
-    && res[2] === array[5]
+    && res[0] === text[3]
+    && res[1] === text[4]
+    && res[2] === text[5]
     && items.length === 6
-    && items[0] === array[0]
-    && items[1] === array[1]
-    && items[2] === array[2]
-    && items[3] === array[3]
-    && items[4] === array[4]
-    && items[5] === array[5])
+    && items[0] === text[0]
+    && items[1] === text[1]
+    && items[2] === text[2]
+    && items[3] === text[3]
+    && items[4] === text[4]
+    && items[5] === text[5])
+    success('test ok')
+else
+    fail('test ko')
+
+
+
+describe('case 2')
+
+var numbers = [10, -10, 20, -20, 30, -30, 40, -40]
+var items = []
+
+var res = filter(numbers, function (element, index) {
+    items[index] = element
+
+    return element > 0
+})
+
+if (res instanceof Array
+    && res.length === 4
+    && res[0] === numbers[0]
+    && res[1] === numbers[2]
+    && res[2] === numbers[4]
+    && res[3] === numbers[6]
+    && items.length === 8
+    && items[0] === numbers[0]
+    && items[1] === numbers[1]
+    && items[2] === numbers[2]
+    && items[3] === numbers[3]
+    && items[4] === numbers[4]
+    && items[5] === numbers[5]
+    && items[6] === numbers[6]
+    && items[7] === numbers[7])
     success('test ok')
 else
     fail('test ko')
