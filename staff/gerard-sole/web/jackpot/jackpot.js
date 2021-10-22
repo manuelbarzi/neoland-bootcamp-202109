163d0d5a975851ccc34b4fadc0cbb2f710a785fb
gerard sole <gerardsoclu@gmail.com>
2021-10-22 09:20:58 +0200

    add login

--- staff/gerard-sole/web/jackpot/jackpot.js
+++ staff/gerard-sole/web/jackpot/jackpot.js
@@ -1,39 +1,41 @@
+describe('JACKPOT')
 function jackpot(initCash) {
     var _initCash = initCash
-
     return {
         bet: function (ourNumber, ourBet) {
             if (_initCash >= ourBet && ourNumber === Math.floor(Math.random()*6)){
-                console.log('Has acertado!')
-                _initCash = _initCash + ourBet * 2;
+                win('Has ganado!')
+                _initCash = _initCash + ourBet * 4;
             }else if(_initCash >= ourBet ){
-                console.log('Has perdido!')
+                lost('Has perdido!')
                 _initCash = _initCash - ourBet;
             }else{
-                console.log('No tienes dinero para apostar, tienes '+ _initCash +'. Usa play.addCash(cantidad)')         
+                describe('No tienes dinero para apostar, tienes '+ _initCash +'. Usa play.addCash(cantidad)')         
             }
         },
 
         addCash: function (cash) {
             _initCash += cash
-            console.log('Se ha añadido '+ cash +' a tu cuenta. Saldo: '+ _initCash)
+            describe('Se ha añadido '+ cash +' a tu cuenta. Saldo: '+ _initCash)
         },
 
         checkOut: function(cash) {
             if(cash<_initCash){
                 _initCash -= cash
-                console.log('Se ha extraido '+ cash +' de tu cuenta. Saldo: '+ _initCash)
+                describe('Se ha extraido '+ cash +' de tu cuenta. Saldo: '+ _initCash)
             }
             else
-             console.log('No tienes tanto dinero. Saldo: '+ _initCash)
+             describe('No tienes tanto dinero. Saldo: '+ _initCash)
 
         },
         
         myCash: function() {
-            console.log('your total money its ' + _initCash)
+            describe('your total money its ' + _initCash)
         }
     }
 }
 
-var play = jackpot(100)
+var player1 = jackpot(100)
+var player2 = jackpot(100)
+
 
