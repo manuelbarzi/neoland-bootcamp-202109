fc810506732450ea7658cbe334ae63c230972ccc
gerard sole <gerardsoclu@gmail.com>
2021-10-25 18:01:48 +0200

    boost demo app

--- staff/gerard-sole/web/demo-app/main.js
+++ staff/gerard-sole/web/demo-app/main.js
@@ -1,22 +1,18 @@
-//data
-var users = []
-
-
-
-//views
-
 var landingContainer = document.querySelector('.landing')
 var signupContainer = document.querySelector('.signup')
 var signinContainer = document.querySelector('.signin')
 var postSignupContainer = document.querySelector('.post-signup')
-var homepage = document.querySelector('.homepage')
-var nameSpan = homepage.querySelector('.name')
+var home = document.querySelector('.home')
+var profile = document.querySelector('.profile')
+var unregister = document.querySelector('.unregister')
+
+var token
 
 var landingButtons = landingContainer.querySelectorAll('button')
 
 var signupButton = landingButtons[1]
 
-signupButton.onclick = function() {
+signupButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signupContainer.classList.remove('container--off')
@@ -24,16 +20,15 @@ signupButton.onclick = function() {
 
 var landingSigninButton = landingButtons[0]
 
-landingSigninButton.onclick = function() {
+landingSigninButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signinContainer.classList.remove('container--off')
 }
-var signupButtons = signupContainer.querySelectorAll('button')
 
-var signupSigninButton = signupButtons[0]
+var signupSigninButton = signupContainer.querySelector('button')
 
-signupSigninButton.onclick = function(event) {
+signupSigninButton.onclick = function (event) {
     event.preventDefault()
 
     signupContainer.classList.add('container--off')
@@ -41,11 +36,9 @@ signupSigninButton.onclick = function(event) {
     signinContainer.classList.remove('container--off')
 }
 
-var signinButtons = signinContainer.querySelectorAll('button')
+var signinSignupButton = signinContainer.querySelector('button')
 
-var signinSignupButton = signinButtons[0]
-
-signinSignupButton.onclick = function(event) {
+signinSignupButton.onclick = function (event) {
     event.preventDefault()
 
     signinContainer.classList.add('container--off')
@@ -53,15 +46,72 @@ signinSignupButton.onclick = function(event) {
     signupContainer.classList.remove('container--off')
 }
 
-signinContainer.onsubmit =function(event){
+signupContainer.onsubmit = function (event) {
     event.preventDefault()
+
+    var inputs = signupContainer.querySelectorAll('input')
+
+    var nameInput = inputs[0]
+    var usernameInput = inputs[1]
+    var passwordInput = inputs[2]
+
+    var name = nameInput.value
+    var username = usernameInput.value
+    var password = passwordInput.value
+
+    if (!name.length) return alert('name is empty')
+    if (!username.length) return alert('username is empty')
+    if (!password.length) return alert('password is empty')
+
+    var xhr = new XMLHttpRequest
+
+    xhr.onload = function () {
+        var status = xhr.status
+
+        if (status === 409 || status === 400) {
+            var response = xhr.responseText
+
+            var message = response.slice(10, -2)
+
+            return alert(message)
+        }
+
+        if (status === 201) {
+            signupContainer.reset()
+
+            signupContainer.classList.add('container--off')
+
+            postSignupContainer.classList.remove('container--off')
+        }
+    }
+
+    xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    var body = '{ "name": "' + name + '", "username": "' + username + '", "password": "' + password + '" }'
+
+    xhr.send(body)
+}
+
+var postSignupSigninButton = postSignupContainer.querySelector('button')
+
+postSignupSigninButton.onclick = function () {
+    postSignupContainer.classList.add('container--off')
+
+    signinContainer.classList.remove('container--off')
+}
+
+signinContainer.onsubmit = function (event) {
+    event.preventDefault()
+
     var inputs = signinContainer.querySelectorAll('input')
 
-    var usernameInput=inputs[0]
-    var passwordInput=inputs[1]
+    var usernameInput = inputs[0]
+    var passwordInput = inputs[1]
 
-    var username= usernameInput.value
-    var password =passwordInput.value
+    var username = usernameInput.value
+    var password = passwordInput.value
 
     if (!username.length) return alert('username is empty')
     if (!password.length) return alert('password is empty')
@@ -71,29 +121,39 @@ signinContainer.onsubmit =function(event){
     xhr.onload = function () {
         var status = xhr.status
 
-        if (status === 401) return alert ('wrong credentials')
+        if (status === 401) {
+            var response = xhr.responseText
+
+            var message = response.slice(10, -2)
+
+            return alert(message)
+        }
 
         if (status === 200) {
             var response = xhr.responseText
 
-            var token = response.slice(10, -2)
+            token = response.slice(10, -2)
+
+            signinContainer.reset()
 
             var xhr2 = new XMLHttpRequest
 
-            xhr2.onload = function() {
+            xhr2.onload = function () {
                 var response = xhr2.responseText
 
-                var name = response.slice(9, response.indexOf(',') -1)
-
-                signinContainer.reset()
+                var name = response.slice(9, response.indexOf(',') - 1)
+                
+                var nameSpan = home.querySelector('.name')
+                
+                nameSpan.innerText = name
 
                 signinContainer.classList.add('container--off')
 
-                nameSpan.innerText = name 
-                
-                homepage.classList.remove('container--off')
+                home.classList.remove('container--off')
             }
+
             xhr2.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
             xhr2.setRequestHeader('Authorization', 'Bearer ' + token)
 
             xhr2.send()
@@ -109,58 +169,186 @@ signinContainer.onsubmit =function(event){
     xhr.send(body)
 }
 
+var homeButtons = home.querySelectorAll('button')
 
-signupContainer.onsubmit = function(event) {
-    event.preventDefault()
+var homeProfileButton = homeButtons[0]
 
-    var inputs = signupContainer.querySelectorAll('input')
+var profileForm = profile.querySelector('form')
 
-    var nameInput = inputs[0]
-    var usernameInput = inputs[1]
-    var passwordInput = inputs[2]
-    var emailInput = inputs[3]
+var profileForm2 = profile.querySelectorAll('form')[1]
 
-    var name = nameInput.value
-    var username = usernameInput.value
-    var password = passwordInput.value
-    var email = emailInput.value
+homeProfileButton.onclick = function() {
+    home.classList.add('container--off')
 
-    if (!name.length) return alert('name is empty')
-    if (!username.length) return alert('username is empty')
-    if (!password.length) return alert('password is empty')
-    if (!email.length) return alert('email is empty')
+    profileForm.reset()
 
-    var xhr = new XMLHttpRequest
-
-    xhr.onload = function () {
-        var status = xhr.status
-
-        if (status === 409) return alert('user already exists')
-
-        if (status === 201) {
-            signupContainer.reset()
-
-            signupContainer.classList.add('container--off')
-
-            postSignupContainer.classList.remove('container--off')
-        }
-    }
-
-
-   xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users') 
-
-   xhr.setRequestHeader('Content-Type', 'application/json')
-
-   var body = '{ "name": "' + name + '", "username": "' + username + '", "password": "' + password + '" }'
-
-   xhr.send(body)
+    profile.classList.remove('container--off')
 }
 
-var postSignupSigninButton = postSignupContainer.querySelector('button')
+var homeSignoutButton = homeButtons[1]
 
-postSignupSigninButton.onclick = function() {
-    postSignupContainer.classList.add('container--off')
+homeSignoutButton.onclick = function() {
+    home.classList.add('container--off')
 
     signinContainer.classList.remove('container--off')
 }
 
+var profileBackButton = profileForm.querySelector('button')
+
+profileBackButton.onclick = function(event) {
+    event.preventDefault()
+
+    profile.classList.add('container--off')
+
+    home.classList.remove('container--off')
+}
+
+profileForm.onsubmit = function (event) {
+    event.preventDefault()
+    
+    var inputs = profileForm.querySelectorAll('input')
+
+    var oldPasswordInput = inputs[0]
+    var passwordInput = inputs[1]
+
+    var oldPassword = oldPasswordInput.value
+    var password = passwordInput.value
+
+    var xhr = new XMLHttpRequest
+
+    xhr.onload = function() {
+        var status = xhr.status
+
+        if (status === 400 || status === 401) {
+            var response = xhr.responseText
+
+            var message = response.slice(10, -2)
+
+            return alert(message)
+        }
+
+        if (status === 204) {
+            profile.classList.add('container--off')
+
+            profileForm.reset()
+
+            home.classList.remove('container--off')
+        }
+    }
+
+    xhr.open('PATCH', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+    
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    var body = '{ "oldPassword": "' + oldPassword + '", "password" : "' + password + '" }'
+    
+    xhr.send(body)
+
+    // profileForm2.onsubmit = function (event) {
+    //     event.preventDefault()
+        
+    //     var inputs = profileForm.querySelectorAll('input')
+    
+    //     var usernameInput = inputs[0]
+    //     var passwordInput = inputs[1]
+    
+    //     var username = usernameInput.value
+    //     var password = passwordInput.value
+    
+    //     var xhr = new XMLHttpRequest
+    
+    //     xhr.onload = function() {
+    //         var status = xhr.status
+    
+    //         if (status === 400 || status === 401) {
+    //             var response = xhr.responseText
+    
+    //             var message = response.slice(10, -2)
+    
+    //             return alert(message)
+    //         }
+    
+    //         if (status === 204) {
+    //             profile.classList.add('container--off')
+    
+    //             profileForm.reset()
+    
+    //             home.classList.remove('container--off')
+    //         }
+    //     }
+    
+    //     xhr.open('PATCH', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+    
+    //     xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+        
+    //     xhr.setRequestHeader('Content-Type', 'application/json')
+    
+    //     var body = '{ "oldPassword": "' + oldPassword + '", "password" : "' + password + '" }'
+        
+    //     xhr.send(body)
+}
+
+
+var profileUnregisterButton = profile.querySelector('.profile>button')
+
+profileUnregisterButton.onclick = function() {
+    profile.classList.add('container--off')
+
+    unregister.classList.remove('container--off')
+}
+
+var unregisterBackButton = unregister.querySelector('button')
+
+var unregisterForm = unregister.querySelector('form')
+
+unregisterBackButton.onclick = function(event) {
+    event.preventDefault()
+
+    unregister.classList.add('container--off')
+
+    unregisterForm.reset()
+
+    profile.classList.remove('container--off')
+}
+
+unregisterForm.onsubmit = function(event) {
+    event.preventDefault()
+
+    var passwordInput = unregisterForm.querySelector('input')
+
+    var password = passwordInput.value
+
+    var xhr = new XMLHttpRequest
+
+    xhr.onload = function() {
+        var status = xhr.status
+
+        if (status === 400 || status === 401) {
+            var response = xhr.responseText
+
+            var message = response.slice(10, -2)
+
+            return alert(message)
+        }
+
+        if(status === 204) {
+            unregister.classList.add('container--off')
+
+            unregisterForm.reset()
+
+            landingContainer.classList.remove('container--off')
+        }
+    }
+
+    xhr.open('DELETE', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    var body = '{ "password" : "' + password + '" }'
+
+    xhr.send(body)
+}
