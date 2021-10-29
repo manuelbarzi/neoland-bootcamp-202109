1104c5d1b87e157d23e17756aad3817bdc9b1390
Krethz <andreu1986@gmail.com>
2021-10-29 14:47:44 +0200

    updating cchanges

--- staff/andreu-martinez/web/demo-app/main.js
+++ staff/andreu-martinez/web/demo-app/main.js
@@ -1,22 +1,51 @@
 //data
 var users = []
-
-
+var token
 
 //views
 
 var landingContainer = document.querySelector('.landing')
+var profileContainer = document.querySelector('.profile')
+var unregisterContainer = document.querySelector('.unregister')
 var signupContainer = document.querySelector('.signup')
 var signinContainer = document.querySelector('.signin')
 var postSignupContainer = document.querySelector('.post-signup')
-var homepage = document.querySelector('.homepage')
-var nameSpan = homepage.querySelector('.name')
+var home = document.querySelector('.home')
+var nameSpan = home.querySelector('.name')
+var spinnerContainer = document.querySelector(".spinner")
 
-var landingButtons = landingContainer.querySelectorAll('button')
+
+if(!sessionStorage.token){
+    spinnerContainer.classList.add('container--off')
+    landingContainer.classList.remove('container--off')
+
+}else{
+    try{
+        retrieveUser(sessionStorage.token, function(error, user){
+
+            landingContainer.classList.add('container--off')
+            spinnerContainer.classList.add('container--off')
+            if(error) return alert(error.message)
+
+            var name = user.name
+            var nameSpan =home.querySelector('.name')
+
+            nameSpan.innerText = name
+            
+            home.classList.remove('container--off')
+        })
+
+    }catch(error){
+        alert(error.message)
+    }
+}
+
+
+var landingButtons = landingContainer.querySelectorAll('a')
 
 var signupButton = landingButtons[1]
 
-signupButton.onclick = function() {
+signupButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signupContainer.classList.remove('container--off')
@@ -24,16 +53,19 @@ signupButton.onclick = function() {
 
 var landingSigninButton = landingButtons[0]
 
-landingSigninButton.onclick = function() {
+landingSigninButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signinContainer.classList.remove('container--off')
 }
-var signupButtons = signupContainer.querySelectorAll('button')
 
-var signupSigninButton = signupButtons[0]
 
-signupSigninButton.onclick = function(event) {
+var signupButton = signupContainer.querySelector('button')
+
+var signupLink = signupContainer.querySelector('a')
+
+
+signupLink.onclick = function (event) {
     event.preventDefault()
 
     signupContainer.classList.add('container--off')
@@ -45,7 +77,9 @@ var signinButtons = signinContainer.querySelectorAll('button')
 
 var signinSignupButton = signinButtons[0]
 
-signinSignupButton.onclick = function(event) {
+var signinLink = signinContainer.querySelector('a')
+
+signinLink.onclick = function (event) {
     event.preventDefault()
 
     signinContainer.classList.add('container--off')
@@ -53,64 +87,77 @@ signinSignupButton.onclick = function(event) {
     signupContainer.classList.remove('container--off')
 }
 
-signinContainer.onsubmit =function(event){
+
+
+signinContainer.onsubmit = function (event) {
     event.preventDefault()
+
     var inputs = signinContainer.querySelectorAll('input')
 
-    var usernameInput=inputs[0]
-    var passwordInput=inputs[1]
+    var usernameInput = inputs[0]
+    var passwordInput = inputs[1]
 
-    var username= usernameInput.value
-    var password =passwordInput.value
+    var username = usernameInput.value
+    var password = passwordInput.value
 
-    if (!username.length) return alert('username is empty')
-    if (!password.length) return alert('password is empty')
+    signinContainer.classList.add('container--off')
+    spinnerContainer.classList.remove('container--off')
 
-    var xhr = new XMLHttpRequest
+    try {
+        signinUser(username, password, function (error, token) {
+            if (error) {
+                // alert(error.message)
+                injectableModal("template-modal", "Error", err.message);
+                // spinnerContainer.classList.add('container--off')
+                // signinContainer.classList.remove('container--off')
 
-    xhr.onload = function () {
-        var status = xhr.status
-
-        if (status === 401) return alert ('wrong credentials')
-
-        if (status === 200) {
-            var response = xhr.responseText
-
-            var token = response.slice(10, -2)
-
-            var xhr2 = new XMLHttpRequest
-
-            xhr2.onload = function() {
-                var response = xhr2.responseText
-
-                var name = response.slice(9, response.indexOf(',') -1)
-
-                signinContainer.reset()
-
-                signinContainer.classList.add('container--off')
-
-                nameSpan.innerText = name 
-                
-                homepage.classList.remove('container--off')
+                return
             }
-            xhr2.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
-            xhr2.setRequestHeader('Authorization', 'Bearer ' + token)
 
-            xhr2.send()
-        }
+            sessionStorage.token = token
+
+            signinContainer.reset()
+
+            try {
+                retrieveUser(sessionStorage.token, function (error, user) {
+                    if (error) {
+                        alert(error.message)
+                        injectableModal("template-modal", "Error", err.message);
+                        // spinnerContainer.classList.add('container--off')
+                        // signinContainer.classList.remove('container--off')
+        
+                        return
+                    }else{
+
+                    }
+
+                    // var name = user.name
+
+                    // spinnerContainer.classList.add('container--off')
+
+                    // var nameSpan = home.querySelector('.name')
+
+                    // nameSpan.innerText = name
+
+                    // home.classList.remove('container--off')
+                })
+            } catch (error) {
+                alert(error.message)
+
+                // spinnerContainer.classList.add('container--off')
+                // signinContainer.classList.remove('container--off')
+            }
+        })
+    } catch (error) {
+        // alert(error.message)
+        injectableModal("template-modal", "Error", err.message);
+        // spinnerContainer.classList.add('container--off')
+        // signinContainer.classList.remove('container--off')
     }
-
-    xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users/auth')
-
-    xhr.setRequestHeader('Content-Type', 'application/json')
-
-    var body = '{ "username": "' + username + '", "password": "' + password + '" }'
-
-    xhr.send(body)
 }
 
 
-signupContainer.onsubmit = function(event) {
+signupContainer.onsubmit = function (event) {
     event.preventDefault()
 
     var inputs = signupContainer.querySelectorAll('input')
@@ -118,48 +165,189 @@ signupContainer.onsubmit = function(event) {
     var nameInput = inputs[0]
     var usernameInput = inputs[1]
     var passwordInput = inputs[2]
-    var emailInput = inputs[3]
 
     var name = nameInput.value
     var username = usernameInput.value
     var password = passwordInput.value
-    var email = emailInput.value
 
-    if (!name.length) return alert('name is empty')
-    if (!username.length) return alert('username is empty')
-    if (!password.length) return alert('password is empty')
-    if (!email.length) return alert('email is empty')
+    signupContainer.classList.add('container--off')
+    spinnerContainer.classList.remove('container--off')
 
-    var xhr = new XMLHttpRequest
+    try {
+        signupUser(name, username, password, function (error) {
+            if (error) {
+                // alert(error.message)
+                injectableModal("template-modal", "Error", err.message);
+                signinContainer.reset();
+                // spinnerContainer.classList.add('container--off')
+                // signupContainer.classList.remove('container--off')
 
-    xhr.onload = function () {
-        var status = xhr.status
+                return
+            }
 
-        if (status === 409) return alert('user already exists')
-
-        if (status === 201) {
             signupContainer.reset()
 
-            signupContainer.classList.add('container--off')
+            spinnerContainer.classList.add('container--off')
 
             postSignupContainer.classList.remove('container--off')
-        }
+
+        })
+    } catch (error) {
+        // alert(error.message)
+        injectableModal("template-modal", "Error", err.message);
+        spinnerContainer.classList.add('container--off')
+        signupContainer.classList.remove('container--off')
     }
 
-
-   xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users') 
-
-   xhr.setRequestHeader('Content-Type', 'application/json')
-
-   var body = '{ "name": "' + name + '", "username": "' + username + '", "password": "' + password + '" }'
-
-   xhr.send(body)
 }
 
 var postSignupSigninButton = postSignupContainer.querySelector('button')
 
-postSignupSigninButton.onclick = function() {
-    postSignupContainer.classList.add('container--off')
-
+postSignupSigninButton.onclick = function () {
     signinContainer.classList.remove('container--off')
+    postSignupContainer.classList.add('container--off')
+}
+
+var homeButton = home.querySelectorAll('button')
+
+homeButton[0].onclick = function () {
+    home.classList.add('container--off')
+    profileContainer.classList.remove('container--off')
+}
+
+homeButton[1].onclick = function () {
+    delete sessionStorage.token
+    home.classList.add('container--off')
+    signinContainer.classList.remove('container--off')
+}
+
+
+/// profile form
+
+var buttonsProfile = profileContainer.querySelectorAll('button')
+
+buttonsProfile[0].onclick = function (event) {
+    event.preventDefault()
+    profileContainer.classList.add('container--off')
+    home.classList.remove('container--off')
+
+}
+
+buttonsProfile[2].onclick = function (event) {
+    event.preventDefault()
+    profileContainer.classList.add('container--off')
+    unregisterContainer.classList.remove('container--off')
+}
+
+var profileForm = profileContainer.querySelector('form')
+
+profileForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var inputs = profileForm.querySelectorAll('input')
+
+    var oldPasswordInput = inputs[0]
+    var passwordInput = inputs[1]
+
+    var oldPassword = oldPasswordInput.value
+    var password = passwordInput.value
+
+    try {
+        updateUserPassword(sessionStorage.token, oldPassword, password, function (error) {
+            if (error) return alert(error.message)
+
+            profileContainer.classList.add('container--off')
+
+            profileForm.reset()
+
+            home.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+
+
+/// reset password form
+
+var unregister = unregisterContainer.querySelectorAll('button')
+
+unregister[0].onclick = function (event) {
+    event.preventDefault()
+    profileContainer.classList.remove('container--off')
+    unregisterContainer.classList.add('container--off')
+
+}
+
+var unregisterForm = unregisterContainer.querySelector('form')
+
+unregisterContainer.onsubmit = function (event) {
+    event.preventDefault()
+
+    var passwordInput=unregisterContainer.querySelector('input')
+
+    var password = passwordInput.value
+    
+    try {
+        unregisterUser(sessionStorage.token, password, function (error) {
+            if (error) return alert(error.message)
+
+            unregisterContainer.classList.add('container--off')
+
+            unregisterForm.reset()
+
+            landingContainer.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+var homeSearchForm = home.querySelector('.home__search')
+var homeResultsList = home.querySelector('.home__results')
+
+homeSearchForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var queryInput = homeSearchForm.querySelector('#query')
+
+    var query = queryInput.value
+
+    try {
+        searchVehicles(query, function (error, vehicles) {
+            if (error) return alert(error.message)
+
+            homeResultsList.innerHTML = ''
+
+            vehicles.forEach(function (vehicle) {
+                var item = document.createElement('li')
+
+                var image = document.createElement('img')
+                image.src = vehicle.thumbnail
+
+                var title = document.createElement('h2')
+                title.innerText = vehicle.name
+
+                var price = document.createElement('span')
+                price.innerText = vehicle.price + ' $'
+
+                item.append(image, title, price)
+
+                item.classList.add('home__result')
+
+                item.onclick = function() {
+                    alert(vehicle.id)
+
+                    // TODO call retrieveVehicle(...) and so on... ,)
+                }
+
+                homeResultsList.append(item)
+            })
+        })
+    } catch (error) {
+        alert(error.message)
+    }
 }
--- staff/andreu-martinez/web/demo-app/main_manu.js
+++ staff/andreu-martinez/web/demo-app/main_manu.js
@@ -1,24 +1,42 @@
-// data
-
-var users = [{
-    name: 'Pepito Grillo',
-    username: 'pepigri@mail.com',
-    password: '123123123'
-}]
-
-// views
-
 var landingContainer = document.querySelector('.landing')
 var signupContainer = document.querySelector('.signup')
 var signinContainer = document.querySelector('.signin')
 var postSignupContainer = document.querySelector('.post-signup')
 var homeContainer = document.querySelector('.home')
+var profileContainer = document.querySelector('.profile')
+var unregisterContainer = document.querySelector('.unregister')
+var spinnerContainer = document.querySelector('.spinner')
+
+if (!sessionStorage.token) {
+    spinnerContainer.classList.add('container--off')
+
+    landingContainer.classList.remove('container--off')
+} else {
+    try {
+        retrieveUser(sessionStorage.token, function (error, user) {
+            if (error) return alert(error.message)
+
+            var name = user.name
+
+            var nameSpan = homeContainer.querySelector('.name')
+
+            nameSpan.innerText = name
+
+            spinnerContainer.classList.add('container--off')
+
+            homeContainer.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
 
 var landingButtons = landingContainer.querySelectorAll('button')
 
 var signupButton = landingButtons[1]
 
-signupButton.onclick = function() {
+signupButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signupContainer.classList.remove('container--off')
@@ -26,17 +44,15 @@ signupButton.onclick = function() {
 
 var landingSigninButton = landingButtons[0]
 
-landingSigninButton.onclick = function() {
+landingSigninButton.onclick = function () {
     landingContainer.classList.add('container--off')
 
     signinContainer.classList.remove('container--off')
 }
 
-var signupButtons = signupContainer.querySelectorAll('button')
+var signupSigninButton = signupContainer.querySelector('button')
 
-var signupSigninButton = signupButtons[0]
-
-signupSigninButton.onclick = function(event) {
+signupSigninButton.onclick = function (event) {
     event.preventDefault()
 
     signupContainer.classList.add('container--off')
@@ -44,11 +60,9 @@ signupSigninButton.onclick = function(event) {
     signinContainer.classList.remove('container--off')
 }
 
-var signinButtons = signinContainer.querySelectorAll('button')
+var signinSignupButton = signinContainer.querySelector('button')
 
-var signinSignupButton = signinButtons[0]
-
-signinSignupButton.onclick = function(event) {
+signinSignupButton.onclick = function (event) {
     event.preventDefault()
 
     signinContainer.classList.add('container--off')
@@ -56,7 +70,7 @@ signinSignupButton.onclick = function(event) {
     signupContainer.classList.remove('container--off')
 }
 
-signupContainer.onsubmit = function(event) {
+signupContainer.onsubmit = function (event) {
     event.preventDefault()
 
     var inputs = signupContainer.querySelectorAll('input')
@@ -69,38 +83,43 @@ signupContainer.onsubmit = function(event) {
     var username = usernameInput.value
     var password = passwordInput.value
 
-    if (!name.length) return alert('name is empty')
-    if (!username.length) return alert('username is empty')
-    if (!password.length) return alert('password is empty')
-
-    var user = {
-        name: name,
-        username: username,
-        password: password
-    }
-
-    users.push(user)
-
-    // nameInput.value = ''
-    // usernameInput.value = ''
-    // passwordInput.value = ''
-
-    signupContainer.reset()
-
     signupContainer.classList.add('container--off')
+    spinnerContainer.classList.remove('container--off')
 
-    postSignupContainer.classList.remove('container--off')
+    try {
+        signupUser(name, username, password, function (error) {
+            if (error) {
+                alert(error.message)
+
+                spinnerContainer.classList.add('container--off')
+                signupContainer.classList.remove('container--off')
+
+                return
+            }
+
+            signupContainer.reset()
+
+            spinnerContainer.classList.add('container--off')
+
+            postSignupContainer.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+
+        spinnerContainer.classList.add('container--off')
+        signupContainer.classList.remove('container--off')
+    }
 }
 
 var postSignupSigninButton = postSignupContainer.querySelector('button')
 
-postSignupSigninButton.onclick = function() {
+postSignupSigninButton.onclick = function () {
     postSignupContainer.classList.add('container--off')
 
     signinContainer.classList.remove('container--off')
 }
 
-signinContainer.onsubmit = function(event) {
+signinContainer.onsubmit = function (event) {
     event.preventDefault()
 
     var inputs = signinContainer.querySelectorAll('input')
@@ -111,22 +130,208 @@ signinContainer.onsubmit = function(event) {
     var username = usernameInput.value
     var password = passwordInput.value
 
-    if (!username.length) return alert('username is empty')
-    if (!password.length) return alert('password is empty')
-
-    var user = users.find(function(user) {
-        return user.username === username && user.password === password
-    })
-
-    if (!user) return alert('wrong credentials')
-
-    signinContainer.reset()
-
     signinContainer.classList.add('container--off')
+    spinnerContainer.classList.remove('container--off')
 
-    var nameSpan = homeContainer.querySelector('.name')
+    try {
+        signinUser(username, password, function (error, token) {
+            if (error) {
+                alert(error.message)
 
-    nameSpan.innerText = user.name
+                spinnerContainer.classList.add('container--off')
+                signinContainer.classList.remove('container--off')
+
+                return
+            }
+
+            sessionStorage.token = token
+
+            signinContainer.reset()
+
+            try {
+                retrieveUser(sessionStorage.token, function (error, user) {
+                    if (error) {
+                        alert(error.message)
+
+                        spinnerContainer.classList.add('container--off')
+                        signinContainer.classList.remove('container--off')
+
+                        return
+                    }
+
+                    var name = user.name
+
+                    spinnerContainer.classList.add('container--off')
+
+                    var nameSpan = homeContainer.querySelector('.name')
+
+                    nameSpan.innerText = name
+
+                    homeContainer.classList.remove('container--off')
+                })
+            } catch (error) {
+                alert(error.message)
+
+                spinnerContainer.classList.add('container--off')
+                signinContainer.classList.remove('container--off')
+            }
+        })
+    } catch (error) {
+        alert(error.message)
+
+        spinnerContainer.classList.add('container--off')
+        signinContainer.classList.remove('container--off')
+    }
+}
+
+var homeButtons = homeContainer.querySelectorAll('button')
+
+var homeProfileButton = homeButtons[0]
+
+var profileForm = profileContainer.querySelector('form')
+
+homeProfileButton.onclick = function () {
+    homeContainer.classList.add('container--off')
+
+    profileForm.reset()
+
+    profileContainer.classList.remove('container--off')
+}
+
+var homeSignoutButton = homeButtons[1]
+
+homeSignoutButton.onclick = function () {
+    delete sessionStorage.token
+
+    homeContainer.classList.add('container--off')
+
+    signinContainer.classList.remove('container--off')
+}
+
+var profileBackButton = profileForm.querySelector('button')
+
+profileBackButton.onclick = function (event) {
+    event.preventDefault()
+
+    profileContainer.classList.add('container--off')
 
     homeContainer.classList.remove('container--off')
+}
+
+profileForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var inputs = profileForm.querySelectorAll('input')
+
+    var oldPasswordInput = inputs[0]
+    var passwordInput = inputs[1]
+
+    var oldPassword = oldPasswordInput.value
+    var password = passwordInput.value
+
+    try {
+        updateUserPassword(sessionStorage.token, oldPassword, password, function (error) {
+            if (error) return alert(error.message)
+
+            profileContainer.classList.add('container--off')
+
+            profileForm.reset()
+
+            homeContainer.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+var profileUnregisterButton = profileContainer.querySelector('.profile>button')
+
+profileUnregisterButton.onclick = function () {
+    profileForm.reset()
+
+    profileContainer.classList.add('container--off')
+
+    unregisterContainer.classList.remove('container--off')
+}
+
+var unregisterBackButton = unregisterContainer.querySelector('button')
+
+var unregisterForm = unregisterContainer.querySelector('form')
+
+unregisterBackButton.onclick = function (event) {
+    event.preventDefault()
+
+    unregisterContainer.classList.add('container--off')
+
+    unregisterForm.reset()
+
+    profileContainer.classList.remove('container--off')
+}
+
+unregisterForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var passwordInput = unregisterForm.querySelector('input')
+
+    var password = passwordInput.value
+
+    try {
+        unregisterUser(sessionStorage.token, password, function (error) {
+            if (error) return alert(error.message)
+
+            unregisterContainer.classList.add('container--off')
+
+            unregisterForm.reset()
+
+            landingContainer.classList.remove('container--off')
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+}
+
+var homeSearchForm = homeContainer.querySelector('.home__search')
+var homeResultsList = homeContainer.querySelector('.home__results')
+
+homeSearchForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var queryInput = homeSearchForm.querySelector('#query')
+
+    var query = queryInput.value
+
+    try {
+        searchVehicles(query, function (error, vehicles) {
+            if (error) return alert(error.message)
+
+            homeResultsList.innerHTML = ''
+
+            vehicles.forEach(function (vehicle) {
+                var item = document.createElement('li')
+
+                var image = document.createElement('img')
+                image.src = vehicle.thumbnail
+
+                var title = document.createElement('h2')
+                title.innerText = vehicle.name
+
+                var price = document.createElement('span')
+                price.innerText = vehicle.price + ' $'
+
+                item.append(image, title, price)
+
+                item.classList.add('home__result')
+
+                item.onclick = function() {
+                    alert(vehicle.id)
+
+                    // TODO call retrieveVehicle(...) and so on... ,)
+                }
+
+                homeResultsList.append(item)
+            })
+        })
+    } catch (error) {
+        alert(error.message)
+    }
 }
