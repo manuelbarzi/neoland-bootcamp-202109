a58d39baaa8209a6ef9e25abaf29499d40d9777e
Adrian <eidrian.uct@gmail.com>
2021-11-08 15:29:32 +0100

    new components my-demo-app add

--- staff/my-demo-app/src/components/SignUp.jsx
+++ staff/my-demo-app/src/components/SignUp.jsx
@@ -1,25 +1,31 @@
-function SignUp({onSignUp,onSignIn}) {
+import './SignUp.css';
+
+function SignUp({ params }) {
     return (
-        <form className="signup container container--vertical container--gapped" onSubmit={event=>{
-            event.preventDefault()
-
-            const name = event.target.name.value
-            const username = event.target.username.value
-            const password = event.target.password.value
-
-            onSignUp (name,username,password)
-        }}>
-            <input className="field" type="text" name="name" id="name" placeholder="name" />
-            <input className="field" type="text" name="username" id="username" placeholder="username" />
-            <input className="field" type="password" name="password" id="password" placeholder="password" />
-
-            <div className="container">
-                <button className="button button--medium" onClick={()=>onSignIn()} >Sign in</button>
-                <button className="button button--medium button--dark">Sign up</button>
-            </div>
-        </form>
+        <div className="halfSignUp">
+            <form action="" className="signUp">
+                <p className="signUp__title" >Sign Up</p>
+                <p className="signUp__dontHave">Don't have an account?</p>
+                <div className="labelAndInput">
+                    <label className="label" htmlFor="fullName">Full Name</label>
+                    <input className="input" type="text" name="fullName" id="fullName" placeholder="Ivan Pavlov" />
+                </div>
+                <div className="labelAndInput">
+                    <label className="label" htmlFor="email">Your Email</label>
+                    <input className="input" type="email" name="email" id="email" placeholder="helloworld@mail.com" />
+                </div>
+                <div className="labelAndInput">
+                    <label className="label" htmlFor="password">Password</label>
+                    <input className="input" type="password" name="password" id="password" placeholder="Enter your password" />
+                </div>
+                <div className="labelAndInput">
+                    <label className="label" htmlFor="repeatPassword">Repeat Password</label>
+                    <input className="input" type="password" name="repeatPassword" id="repeatPassword" placeholder="Enter your password" />
+                </div>
+                <button className="btn" type="submit">Sign Up</button>
+            </form>
+        </div>
     )
 }
-
 
 export default SignUp
