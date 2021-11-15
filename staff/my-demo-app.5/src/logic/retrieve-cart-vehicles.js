03e5306e060d4389fde3ab21ed5925ab1fab75da
Sergio Ayala <sergio.ayala51@gmail.com>
2021-11-15 09:31:44 +0100

    new my demo app before create routes

--- staff/my-demo-app.5/src/logic/retrieve-cart-vehicles.js
+++ staff/my-demo-app.5/src/logic/retrieve-cart-vehicles.js
@@ -1,4 +1,4 @@
-function retrieveFavVehicles(token, callback) {
+function retrieveCartVehicles(token, callback) {
     if (typeof token !== 'string') throw new TypeError(`${token} is not a string`)
     if (!/[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)$/.test(token)) throw new Error('invalid token')
 
@@ -7,7 +7,7 @@ function retrieveFavVehicles(token, callback) {
     const xhr = new XMLHttpRequest
 
     xhr.onload = () => {
-        const {status, responseText} = xhr
+        const { status, responseText } = xhr
 
         if (status === 401 || status === 404) {
             const response = JSON.parse(responseText)
@@ -15,49 +15,51 @@ function retrieveFavVehicles(token, callback) {
             callback(new Error(message))
 
         } else if (status === 200) {
+
             const user = JSON.parse(responseText)
 
-            const {favs = []} = user
-            
-            if (favs.length) {
+            const { cart = [] } = user
+
+            if (cart.length) {
                 let count = 0
                 const vehicles = []
 
-                favs.forEach((id, index) => {
+                cart.forEach((item, index) => {
+                    const { id, qty } = item
 
                     const xhr2 = new XMLHttpRequest
 
                     xhr2.onload = () => {
-                        const {status, responseText} = xhr2
+                        const { status, responseText } = xhr2
 
                         if (status === 200) {
+
                             const vehicle = JSON.parse(responseText)
 
-                            if (!vehicle) return callback(new Error(`no vehicle found with id ${id}`))
+                            if (!vehicle) return callback(new Error(`there's any Car with this ${id}`))
 
-                            count++
+                            vehicle.qty = qty
                             vehicles[index] = vehicle
+                            count++
 
-                            if (count === favs.length) {
-                                vehicles.forEach(vehicle => vehicle.isFav = true)
+                            if (count === cart.length) {
                                 callback(null, vehicles)
                             }
                         }
-
                     }
-                    xhr2.open ('GET', `https://b00tc4mp.herokuapp.com/api/hotwheels/vehicles/${id}`)
+
+                    xhr2.open('GET', `https://b00tc4mp.herokuapp.com/api/hotwheels/vehicles/${id}`)
+
                     xhr2.send()
-                    
+
                 });
             } else callback(null, [])
-
         }
     }
 
     xhr.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
     xhr.setRequestHeader('Authorization', `Bearer ${token}`)
     xhr.send()
-
 }
 
-export default retrieveFavVehicles
+export default retrieveCartVehicles
