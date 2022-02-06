75691b5d6ca06742dc12f3641f9618a98df3ea2f
Adrian <eidrian.uct@gmail.com>
2022-02-06 23:28:04 +0100

    delete false commit

--- staff/full-stack/haakon/app/src/App.js
+++ staff/full-stack/haakon/app/src/App.js
@@ -1,53 +1,61 @@
-import React from 'react'
-import { Route } from 'wouter'
+import { useState } from 'react'
 
-import Home from 'pages/Home'
-import SearchGames from 'pages/SearchGames'
-import DetailGame from 'pages/DetailGame'
-import Login from 'pages/Login'
-import Register from 'pages/Register'
+import Landing from './pages/Landing'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Home from './pages/Home'
 
-import { UserContextProvider } from 'context/UserContext'
+import Spinner from './components/Spinner'
+import Modal from './components/Modal'
 
-const App = () => {
+import AppContext from './context/AppContext'
+
+function App() {
+  const [view, setView] = useState(sessionStorage.token ? 'home' : 'landing')
+  const [spinner, setSpinner] = useState(sessionStorage.token ? true : false)
+  const [modal, setModal] = useState(null)
+  const [level, setLevel] = useState(null)
+
+  const goToLanding = () => setView('landing')
+  const goToRegister = () => setView('register')
+  const goToLogin = () => setView('login')
+  const goToHome = () => setView('home')
+
+  const showSpinner = () => setSpinner(true)
+  const hideSpinner = () => setSpinner(false)
+
+  const acceptModal = () => setModal(null)
+
+  const showModal = (message, level = 'error') => {
+    setModal(message)
+    setLevel(level)
+  }
+
+  const resetTokenAndGoToLanding = () => {
+    delete sessionStorage.token
+    setView('landing')
+    setSpinner(false)
+  }
+
   return <>
-    <UserContextProvider>
-      <Route
-        component={Home}
-        path='/'
-      />
-      <Route
-        component={SearchGames}
-        path='/search/:query'
-      />
-      <Route
-        component={DetailGame}
-        path='/game/:id'
-      />
-      <Route
-        component={Login}
-        path='/login'
-      />
-      <Route
-        component={Register}
-        path='/register'
-      />
-    </UserContextProvider>
+    <AppContext.Provider value={{
+      showSpinner,
+      hideSpinner,
+      showModal
+    }}>
+      {view === 'landing' && <Landing goToRegister={goToRegister} goToLogin={goToLogin} />}
+
+      {view === 'register' && <Register goToLogin={goToLogin} />}
+
+      {view === 'login' && <Login goToHome={goToHome} goToRegister={goToRegister} />}
+
+      {view === 'home' && <Home goToLanding={goToLanding} resetTokenAndGoToLanding={resetTokenAndGoToLanding} />}
+
+      {spinner && <Spinner />}
+
+      {modal && <Modal level={level} message={modal} onAccept={acceptModal} />}
+    </AppContext.Provider>
   </>
 }
 
 export default App
-
-// App
-  // Landing
-  // Register
-  // Post-Register
-  // Login
-  // Home
-    // Header
-    // SideBarMenu
-    // GameCard
-    // GameDetail
-    // UnRegister
-    // UserDetail
-    // UserSettings
--- staff/full-stack/haakon/app/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import '../sass/styles.sass'
-
-export default function Spinner() {
-    return (
-        <div className="spinner">
-            <img className="spinner__img" src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" alt="" />
-        </div>
-    )
-}
